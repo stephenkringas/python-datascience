b741c2fef6eca4c2a082748a74b7cad13bbc8efa
Stephen Kringas <stephenkringas@yahoo.com.au>
2018-11-28 12:25:58 +1100

    Week 2 Word Count complete

--- week2/word_cloud.py
+++ week2/word_cloud.py
@@ -1,33 +1,28 @@
 
 import re
+from collections import Counter
 
 # Word cloud dictionary
 dict = dict()
+
 # Regex expression for letters
 regex = re.compile('[^a-zA-Z]')
-
-# Open the stop words not to be included in the word count
-f = open('stopwords.txt', 'r')
-stop_words = f.readlines()
-f.close()
 
 # Open the file containing charles dickens novel and split into individual words
 with open('98-0.txt', 'r') as f:
     for line in f:
         for word in line.split():
+            
             #Strip words of punctuation
             word = word.strip()    
             word = regex.sub('', word)
+            word = word.lower()
 
-            if word not in stop_words:
-                if word in dict:
-                    dict[word] = dict[word] + 1
-                else:
-                    dict.update({word:1}) 
+            if word in dict:
+                dict[word] = dict[word] + 1
+            else:
+                dict.update({word:1})
 
-
-key_max = max(dict.keys(), key=(lambda k: dict[k]))
-key_min = min(dict.keys(), key=(lambda k: dict[k]))
-
-print('Maximum Value: ',dict[key_max])
-print('Minimum Value: ',dict[key_min])
+# Find the top 10 words and print the number of occurances
+for key, value in Counter(dict).most_common(10):
+    print(key, value)
